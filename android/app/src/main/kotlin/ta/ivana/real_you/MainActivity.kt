6fcfe54ab149b01ee6bc1729146d172c701320e0
ivana <ziudith131@gmail.com>
2022-04-29 22:49:16 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/ta/ivana/real_you/MainActivity.kt
@@ -0,0 +1,6 @@
+package ta.ivana.real_you
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:real_you/ui/home_view.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Real_you',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeView(),
+    );
+  }
+}
